e45a3adedd5fcda84dfdb3e8b706cbf55766cd8b
JakubIvacek <xivacek@stuba.sk>
2024-02-23 18:36:24 +0100

    homepagev1
    first version home-page

---
+++ js/own_js.js
@@ -0,0 +1,22 @@
+function createCard(imageSrc, cardText) {
+    return `
+            <li style="padding: 5px;">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top img-fluid" src="${imageSrc}" height="200px" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">${cardText}</p>
+                    </div>
+                </div>
+            </li>
+        `;
+}
+
+function printCards(){
+    let cardsHTML = '<ul class="list-unstyled d-flex flex-wrap">';
+    for (let i = 1; i <= 8; i++) {
+        cardsHTML += createCard("img/shirt-black.jpg", "Description : Card " + i);
+    }
+    cardsHTML += '</ul>';
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.innerHTML = cardsHTML;
+}
