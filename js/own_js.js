29f7208bd15cdea4c953b18888c2265527f43998
JakubIvacek <xivacek@stuba.sk>
2024-02-24 17:33:11 +0100

    homepageupdate
    Skoro done homepage este dizajn porobit poriadne

--- js/own_js.js
+++ js/own_js.js
@@ -12,11 +12,12 @@ function createCard(imageSrc, cardText) {
 }
 
 function printCards(){
-    let cardsHTML = '<ul class="list-unstyled d-flex flex-wrap">';
-    for (let i = 1; i <= 8; i++) {
-        cardsHTML += createCard("img/shirt-black.jpg", "Description : Card " + i);
-    }
-    cardsHTML += '</ul>';
+    //let cardsHTML = '<ul class="list-unstyled d-flex flex-wrap">';
+    //for (let i = 1; i <= 8; i++) {
+    //    cardsHTML += createCard("img/shirt-black.jpg", "Description : Card " + i);
+    //}
+    //cardsHTML += '</ul>';
+    let cardsHTML = createCarousel(["img/shirt-black.jpg","img/shirt-black.jpg","img/shirt-black.jpg"],"Description : Card " + i)
     const cardContainer = document.getElementById('cardContainer');
     cardContainer.innerHTML = cardsHTML;
 }
